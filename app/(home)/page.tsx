16c02e90c0dd8acd4d0b1e06ed0309577542a60a
surajjayraman <suraj.jayraman@gmail.com>
2024-06-13 18:40:16 -0400

    Update home page with clerk UserButton

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
