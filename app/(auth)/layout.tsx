cbc6dd1cee4f8a64bbb06022e0c96b91f0475b22
surajjayraman <suraj.jayraman@gmail.com>
2024-06-13 18:18:21 -0400

    Update auth styles

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className="h-full flex items-center justify-center">{children}</div>
   )
 }
 
