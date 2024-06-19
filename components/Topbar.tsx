788b20b345c5be4bd4dcaba61da7c9159799fb8e
surajjayraman <suraj.jayraman@gmail.com>
2024-06-19 12:39:09 -0400

    Update search input to TopBar

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Search } from "lucide-react";
 
 const Topbar = () => {
   return (
@@ -8,12 +9,14 @@ const Topbar = () => {
         <Image src="/logo.png" alt="logo" width={100} height={100} />
       </Link>
       <div className="max-md:hidden w-[400px] rounded-full flex">
-      <input
+        <input
           className="flex-grow bg-[#FFF8EB] rounded-l-full border-none outline-none text-sm pl-4 py-3"
           placeholder="Search for courses"
         />
-        
-        </div>
+        <button className="bg-[#FDAB04] rounded-r-full border-none outline-none cursor-pointer px-4 py-3 hover:bg-[#FDAB04]/80">
+          <Search className="h-4 w-4" />
+        </button>
+      </div>
     </div>
   );
 };
