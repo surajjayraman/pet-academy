79e6d7da4e7ec1f8a938ffea4240cfa30b1eff5a
surajjayraman <suraj.jayraman@gmail.com>
2024-06-19 12:20:00 -0400

    Update component Topbar

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <div className="flex justify-between items-center p-4">
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={100} height={100} />
+      </Link>
+    </div>
+  );
+};
 
-export default Topbar
+export default Topbar;
