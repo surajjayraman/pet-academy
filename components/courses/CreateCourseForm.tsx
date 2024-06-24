9df6d49dbdbe5bd4b7d28233ac656424ba3a8ebf
surajjayraman <suraj.jayraman@gmail.com>
2024-06-24 12:40:43 -0400

    Update CreateCourseForm component with zod schema

--- components/courses/CreateCourseForm.tsx
+++ components/courses/CreateCourseForm.tsx
@@ -1,3 +1,12 @@
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+
 const CreateCourseForm = () => {
   return (
     <div>CreateCourseForm</div>
