c7f664e0bb5e4690f0f6f32b8787b5eb4b9dcdcb
surajjayraman <suraj.jayraman@gmail.com>
2024-06-24 13:59:02 -0400

    Update zod schema with categoryId

--- components/courses/CreateCourseForm.tsx
+++ components/courses/CreateCourseForm.tsx
@@ -6,6 +6,9 @@ const formSchema = z.object({
   title: z.string().min(2, {
     message: "Title is required and minimum 2 characters"
   }),
+  categoryId: z.string().min(1, {
+    message: "Category is required"
+  })
 });
 
 const CreateCourseForm = () => {
