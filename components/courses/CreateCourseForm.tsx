2cc8102eb55322ee25be7ae11f85e9eb370f0231
surajjayraman <suraj.jayraman@gmail.com>
2024-06-24 18:04:34 -0400

    Update CreateCourseForm component return with form fields

--- components/courses/CreateCourseForm.tsx
+++ components/courses/CreateCourseForm.tsx
@@ -47,7 +47,29 @@ const CreateCourseForm = () => {
   }
 
 
-  return <div>CreateCourseForm</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default CreateCourseForm;
