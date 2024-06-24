9e3ebdbd68c9e012021dee245e14d0fd08380dba
surajjayraman <suraj.jayraman@gmail.com>
2024-06-24 14:24:36 -0400

    Update useForm hook with default values

--- components/courses/CreateCourseForm.tsx
+++ components/courses/CreateCourseForm.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const formSchema = z.object({
   title: z.string().min(2, {
-    message: "Title is required and minimum 2 characters"
+    message: "Title is required and minimum 2 characters",
   }),
   categoryId: z.string().min(1, {
-    message: "Category is required"
+    message: "Category is required",
   }),
-    subCategoryId: z.string().min(1, {
-        message: "Subcategory is required"
-    }),
+  subCategoryId: z.string().min(1, {
+    message: "Subcategory is required",
+  }),
 });
 
 const CreateCourseForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      categoryId: "",
+      subCategoryId: "",
+    },
+  });
   return <div>CreateCourseForm</div>;
 };
 
