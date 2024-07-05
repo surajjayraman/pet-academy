dcffd69233bfc15082bf998b823de0504743aaf3
surajjayraman <suraj.jayraman@gmail.com>
2024-07-05 10:56:52 -0400

    Add form field to CreateCourseForm for subCategories

--- components/courses/CreateCourseForm.tsx
+++ components/courses/CreateCourseForm.tsx
@@ -31,10 +31,10 @@ const formSchema = z.object({
 
 interface CreateCourseFormProps {
   categories: {
-    label: string // name of category
-    value: string // categoryId
-    subCategories: { label: string, value: string }[]
-  }[]
+    label: string; // name of category
+    value: string; // categoryId
+    subCategories: { label: string; value: string }[];
+  }[];
 }
 
 const CreateCourseForm = ({ categories }: CreateCourseFormProps) => {
@@ -57,10 +57,17 @@ const CreateCourseForm = ({ categories }: CreateCourseFormProps) => {
 
   return (
     <div className="p-10">
-        <h1 className="text-xl font-bold">Let us give some basics for your course</h1>
-        <p className="text-sm mt-3">It is Ok if you cannot think of a good title.</p>
+      <h1 className="text-xl font-bold">
+        Let us give some basics for your course
+      </h1>
+      <p className="text-sm mt-3">
+        It is Ok if you cannot think of a good title.
+      </p>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-10">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 mt-10"
+        >
           <FormField
             control={form.control}
             name="title"
@@ -81,12 +88,35 @@ const CreateCourseForm = ({ categories }: CreateCourseFormProps) => {
               <FormItem className="flex flex-col">
                 <FormLabel>Category</FormLabel>
                 <FormControl>
-                  <Combobox options={categories} {...field}/>
+                  <Combobox options={categories} {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+
+          <FormField
+            control={form.control}
+            name="subCategoryId"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Subcategory</FormLabel>
+                <FormControl>
+                  <Combobox
+                    options={
+                      categories.find(
+                        (category) =>
+                          category.value === form.watch("categoryId")
+                      )?.subCategories || []
+                    }
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
           <Button type="submit">Submit</Button>
         </form>
       </Form>
